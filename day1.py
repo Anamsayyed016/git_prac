355fb03810166a87a6e2879b38d105e7c773e326
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-27 23:11:56 +0530

    prac27

---
+++ day1.py
@@ -0,0 +1,48 @@
+# print('hello world')
+
+# x=10
+# y=300
+# print(x/y)
+
+# x=50
+# y=40
+# print(x>y)
+# print(x<y)
+
+# print(x<=y)
+# print(x>=y)
+
+# print(x==y)
+# print(x!=y)
+
+# x=20
+# y=30
+# z=40
+# print(x<y and y<z)
+
+# str1="Anam Sayyed"
+# print(type(str1))
+# print('t' in str1)
+# print('A' not in str1)
+
+# x=100
+# y=200
+# print(x is y)
+# print(id(x))
+# print(type(x))
+
+# print(x==y)
+# print(x&y)
+# print(x/y)
+
+# print(~x)
+# print(x^y)
+
+x=20
+y=30
+print(id(x),id(y))
+
+x=[20]
+y=[30]
+print(id(x),id(y))
+
