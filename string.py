435d4d24a29fd3124697079203b0628740b81b2f
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-03 12:09:17 +0530

    3pyprac

--- string.py
+++ string.py
@@ -1,12 +1,19 @@
-print(True and False)
-print(True and True)
+# print(True and False)
+# print(True and True)
 
-print(45>25 and 98>24)
-print(45>25 and 98>100)
+# print(45>25 and 98>24)
+# print(45>25 and 98>100)
 
-#membership opreator
+# #membership opreator
 
-string1 = "Welcome in python programming"
+# string1 = "Welcome in python programming"
 
-print("in" in string1)
-print("ni" in string1)
+# print("in" in string1)
+# print("ni" in string1)
+
+str1 = 'Anam Sayyed'
+print(str1.index('m'))
+print(str1.index('y'))
+
+print(str1.index('A',2))
+
