ad24cfb47612d74df841664ba3bb161d8050aff3
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-06 16:50:53 +0530

    6febpyprac

---
+++ 6febstr.py
@@ -0,0 +1,14 @@
+# str1 = 'Anam Sayyed'
+# print(str1.index(0,5))
+
+# li=[10,20,30,'Anam',50,30]
+# print(id(li))
+# print(type(li))
+# print(li.index(10,1))
+
+# Dict:
+
+d1={'name':'anam','age':27,'quali':'BCA'}
+print(max(d1))
+print(len(d1))
+print(min(d1))
--- index.py
+++ index.py
@@ -3,3 +3,5 @@ print(str1.index('m'))
 print(str1.index('y'))
 
 print(str1.index('A',2))
+
+                
