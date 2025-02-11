9ac9f7f1b370a784a82362378a66b54445e15393
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-11 14:00:20 +0530

    11febpyprac

--- 6febstr.py
+++ 6febstr.py
@@ -9,6 +9,26 @@
 # Dict:
 
 d1={'name':'anam','age':27,'quali':'BCA'}
-print(max(d1))
-print(len(d1))
-print(min(d1))
+d2={'name':'anam','age':27,'quali':'MBA'}
+# print(max(d1))
+# print(len(d1))
+# print(min(d1))
+
+# d2=d1.copy()
+# print(d1,d2)
+
+# l1=['name','email,','contatct']
+# d2=dict.fromkeys(l1)
+# print(d2)
+# d3=dict.fromkeys(l1,55)
+
+# print(d1.values())
+# print(d1.items())
+
+d1.setdefault('name','sayyed')
+
+print(d1)
+
+d1.update(d2)
+print(d1)
+
--- set.py
+++ set.py
@@ -32,8 +32,31 @@
 # s1.update((2,90,99))
 # print(s1)
 
-age=(input("enter age =  "))
-if age>=18:
-    print("eligible")
-else:
-    print("not eligible")
+# age=(input("enter age =  "))
+# if age>=18:
+#     print("eligible")
+# else:
+#     print("not eligible")
+
+set1={'Anam','Ali','Khan'}
+set2={10,52,20,60}
+# print(my_set)
+# print(type(my_set))
+
+# print(max(set1),max(set2))
+# print(min(set1),min(set2))
+# print(len(set1),len(set2))
+# print(sum(set2))
+
+# s1={'anam','raj','rahul'}
+# s2={10,20,30,40.5,60}
+# print(max(s1),max(s2))
+
+# METHOD
+
+# ADD:-
+
+set1.add('Sayyed')
+set2.add(41)
+print(set1)
+print(set2)
